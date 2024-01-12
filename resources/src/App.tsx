2db7990947d58227f0548f7bae5b7fd32c2a3660
shaade <s.shaade77@gmail.com>
2024-01-12 16:40:28 +0100

    cleaning

--- resources/src/App.tsx
+++ resources/src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ChangeEvent, useState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [expression, setExpression] = useState<null | string>(null);
+	const [result, setResult] = useState<null | string>(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const handleInput = async (e: ChangeEvent<HTMLInputElement>) => {
+		
+	};
+
+	return (
+		<div className="wrapper stack">
+			<div className="stack">
+				<h4>Input</h4>
+				<input type="text" value={expression || ""} onChange={handleInput} placeholder="5 8 +" />
+			</div>
+
+			<div className="output">
+				<h4>Output</h4>
+				<div className="content"></div>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
