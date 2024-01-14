8a526ada6ab4573b2cf6ba1271f15a273792a1fb
shaade <s.shaade77@gmail.com>
2024-01-14 19:01:28 +0100

    ensure dist folder is always created

--- app/config.py
+++ app/config.py
@@ -1,3 +1,6 @@
 import pathlib
 
 SQLITE_PATH = pathlib.Path(__file__).parent.parent / "database.sqlite"
+
+# making sure the "dist" folder is created because otherwise FastAPI complain
+(pathlib.Path(__file__).parent.parent / "resources" / "dist").mkdir(exist_ok=True)
